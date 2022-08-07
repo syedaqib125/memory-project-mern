8ad39f1e97da18a23ac4157a6027be4f8fc0152c
Syed Aqib <syed.aqib125@gmail.com>
2022-08-07 05:18:56 +0500

    signup logic developed

--- server/index.js
+++ server/index.js
@@ -30,5 +30,5 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnified
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((error) => console.log(error))
 
-// mongoose.set('useFindAndModify', false); noo need of his line on new version
+// mongoose.set('useFindAndModify', false); noo need of this line on new version
 
