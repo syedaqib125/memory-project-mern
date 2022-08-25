b116b20b08df5bf6e287e70349f439a55382936b
Syed Aqib <syed.aqib125@gmail.com>
2022-08-25 21:19:19 +0500

    Revert "heroku deployment settings"
    This reverts commit a5f5a239cc9f5b3ce3dd7422ac7bc0e072603980.

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ import userRoutes from './routes/usersRoutes.js';
 const app = express();
 dotenv.config()
 
-app.use(bodyParser.json({ limit: "300mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "300mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
@@ -20,20 +20,12 @@ app.use('/user',userRoutes);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello to memories API')
+    res.send('Hello to memories API new wala')
 })
 
 // const CONNECTION_URL = 'mongodb+srv://memory_project:memory_project123@cluster0.24khi.mongodb.net/memoryProject?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-  }
-  
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((error) => console.log(error))
