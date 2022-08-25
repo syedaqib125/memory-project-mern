6a869715a605f9dea04a9fde14a5930fcbe54e67
Syed Aqib <syed.aqib125@gmail.com>
2022-08-25 21:24:47 +0500

    clone second branch to main

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "https://memories-project-125.herokuapp.com",
-// });
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://memories-project-125.herokuapp.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
