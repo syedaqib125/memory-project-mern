d8210cf65b0176a447d322b58c92247a03faf2ae
Syed Aqib <syed.aqib125@gmail.com>
2022-09-17 05:08:17 +0500

    pagination, routing
    static pagination implement,
    routing -> if login so not go to auth url and return to posts

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,34 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Container, Grow, Grid } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Container, Grow, Grid, Paper } from "@material-ui/core";
 
-import Posts from '../Posts/Posts';
-import Form from '../Form/Form';
-import { getPosts } from '../../actions/posts';
-import useStyles from './styles';
+import Pagination from "../Pagination";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+import { getPosts } from "../../actions/posts";
+import useStyles from "./styles";
 
 const Home = () => {
-    const [currentId, setCurrentId] = useState(0)
-    const dispatch = useDispatch();
-    const classes = useStyles()
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-    useEffect(() => {
-        dispatch(getPosts())
-    }, [dispatch])
-    return (
-        <Grow in>
-            <Container>
-                <Grid className={classes.mainContainer} container justifyContent='space-between' alignItems='stretch' spacing={3} >
-                    <Grid item xs={12} sm={7}>
-                        <Posts setCurrentId={setCurrentId} />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form currentId={currentId} setCurrentId={setCurrentId} />
-                    </Grid>
-                </Grid>
-            </Container>
-        </Grow>
-    )
-}
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+  return (
+    <Grow in>
+      <Container>
+        <Grid
+          className={classes.mainContainer}
+          container
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper elevation={6}>
+              <Pagination />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
 
-export default Home
+export default Home;
